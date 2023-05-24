aa25060d5077ccab4686145013d105e6a51a0cbd
Yablonskaya Sofya <sofa512@yandex.ru>
2023-05-25 01:50:15 +0300

    fix: добавлена сложность

--- frontend/hackaton-admin2023/src/pages/CreateTest/index.js
+++ frontend/hackaton-admin2023/src/pages/CreateTest/index.js
@@ -2,18 +2,41 @@ import React, {useEffect, useRef, useState} from 'react';
 import classes from './style.module.scss';
 import QuestionComponent from "../../shared/ui/QuestionComponent";
 
+//TODO: сделать для сложности вывод на русском
+
 const CreateTest = () => {
+
 
     // const [components, setComponents] = useState([]);
     const [inputs, setInputs] = useState([])
     const [amount, setAmount] = useState("")
     const [renderInputs, setRenderInputs] = useState(false);
 
+    const [difficultyParams, setDifficulty] = useState("");
+
     const [isAction, setAction] = useState(false);
+
+    const difficulty = [{
+        id: 0,
+        value: "LITE"
+    },
+        {
+            id: 1,
+            value: "INTERMEDIATE"
+        },
+        {
+            id: 2,
+            value: "HARD"
+        }]
 
 
     function saveAnswers() {
         console.log('Ответы сохранены!')
+    }
+
+    function settingDifficulty(event) {
+        setDifficulty(event.target.value);
+        console.log('value is:', event.target.value);
     }
 
     return (
@@ -30,6 +53,16 @@ const CreateTest = () => {
                 <input onChange={(event) => setAmount(event.target.value)}
                        placeholder="Количество вопросов в тесте (цифрой или числом)"/>
                 <div></div>
+                <select id="select-difficulty" className={classes['select-difficulty']} onChange={settingDifficulty}>
+                    {
+                        difficulty.map(type => {
+                            return (
+                                <option key={type.id} id={type.id} value={type.value}>{type.value}</option>
+                            )
+                        })
+                    }
+                </select>
+                <div></div>
                 <button id='quest' className={classes['btn']} onClick={() => {
                     setRenderInputs(true);
                     document.getElementById("quest").disabled = true;
@@ -41,7 +74,9 @@ const CreateTest = () => {
                         Array.from(Array(Number(amount))).map((x, index) => <QuestionComponent
                             key={index}
                             id={index}/>)}
-                    <button id='save-btn' className={isAction ? classes['action-button'] : classes['button']} type="submit" onClick={()=>{saveAnswers()}}>Сохранить ответы</button>
+                    <button id='save-btn'
+                            className={isAction ? classes['action-button'] : classes['button']}
+                            type="submit" onClick={()=>{saveAnswers()}}>Сохранить ответы</button>
                 </form>
             </div>
         </>
